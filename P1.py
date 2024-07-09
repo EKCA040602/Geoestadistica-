d408cdad274f9868086fe2a3c441bad2dfaadaa1
EKCA040602 <esthivencruzayte837@gmail.com>
2024-07-09 16:38:39 -0500

    Generando una clase y parametros

--- P1.py
+++ P1.py
@@ -1,1 +1,8 @@
-
+class Purga():
+    def __init__(self,archivo,excel,limite,NR):
+        self.archivo=archivo
+        self.excel=excel
+        self.limite=limite
+        self.NR=NR
+    
+        
