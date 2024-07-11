d049e6b162bafd9aa7d4c8ab3febc5a28056c659
EKCA040602 <143982463+EKCA040602@users.noreply.github.com>
2024-07-11 00:55:45 -0500

    Update P1.py
    En este dia nos quedamos en el analisis geoespacial de los datos censurados pero que no sabemos si esa poblacion esta esparcida o en si puede analizarse debido a que esta junta.
    
    Para mañana .
    Nuevos parametros. Litologia de la zona y poligono para maximizar la visualizacion.

--- P1.py
+++ P1.py
@@ -6,12 +6,17 @@
 
 import pandas as pd
 import numpy as np
+import geopandas as gpd
 
 DATA=pd.read_excel("/content/DATA_MADRE.xlsx")
 EXCEL=pd.read_excel("/content/VERIFICACION_DE_ELEMENTOS.xlsx")
-NR=input("Alguna de sus columnas esta llena de un simbolo o dos letras en TODA esa columna ,si es asi  INDICA CUAL ES LA LETRA .")
-EP=input("De igual manera ,tienes que identificar cuales elementos seran tus mas importantes sin contabilizar ")
-metodo=input("Colocar el nombre de la digestion a usar : Ejemplo -->  4 acidos")
+metodo="4 acidos"
+limite=45
+NR="N.R."
+EP=["Co","Ag","Mo","Au"]
+
+
+
 class Purga():
     def __init__(self,archivo,excel,metodo,limite,NR,EP):
         self.archivo=archivo
@@ -21,5 +26,69 @@ class Purga():
         self.NR=NR
         self.EP=EP
     def base(self):
-        metodo=self.excel[(self.excel["1ER METODO"]==self.metodo) | (self.excel["2DO METODO"]==self.metodo)]
-        elementos=list(metodo["SIMBOLO"])
+  
+      #Borramos la data que no sirve 
+      columna=list( self.archivo.columns)
+      columna_vacia=[]
+      for i in columna:
+        if  self.archivo[i].dtype=="object" and all (self.archivo[i].isin([self.NR])):
+            columna_vacia.append(i)
+      self.archivo=self.archivo.drop(columna_vacia,axis=1)
+
+      #Usamos un archivo EXCEL con el que vamos a discriminar los elementos que  son aptos.
+
+      metodo=self.excel[(self.excel["1ER METODO"]==self.metodo) | (self.excel["2DO METODO"]==self.metodo)]
+      elementos=list(metodo["SIMBOLO"])
+      self.elementos=elementos
+
+      elementos_guardados=self.archivo.iloc[:,:25].copy()
+
+      #Es bien sabido que se aceptaran otros elementos de modo que serviran como PATHFINDERS.
+
+      for i in self.archivo.iloc[:,25:].columns:
+        if i[:2] in EP:
+          elementos_guardados[i]=self.archivo[i].copy()
+
+      self.elementos_guardados=elementos_guardados.iloc[:,25:].copy()
+
+
+      for x in self.archivo.iloc[:,25:].columns:
+        if x[:2] not in self.elementos:
+          self.archivo=self.archivo.drop(x,axis=1)
+
+      for guardado in self.elementos_guardados.columns:
+        if guardado not in list(self.archivo.iloc[:,25:].columns):
+          self.archivo[guardado]=self.elementos_guardados[guardado].copy()
+
+      #Identificar columnas con poblaciones con bajo limite de deteccion.
+
+      for i in self.archivo.iloc[:,25:]:
+        self.archivo[i] = self.archivo[i].astype(object)
+
+      censura={}
+
+      for i  in self.archivo.iloc[:,25:]:
+        c=0
+        for j in range(0,len(self.archivo)):
+          if (str(self.archivo[i][j]).startswith("<")):
+            c+=1
+        d=round(((c*100)/len(self.archivo)),2)
+
+        censura[i]=d
+
+      for i in censura.keys():
+        if censura.get(i)==100:
+          self.archivo=self.archivo.drop(i,axis=1)
+        elif censura.get(i)>self.limite:
+          ubicacion=self.archivo.iloc[:,:25].copy()
+          ubicacion[i]=self.archivo[i]
+
+          for y in range(0,len(ubicacion)):
+            if  str(ubicacion[i][y]).startswith("<"):
+              ubicacion=ubicacion.drop(y)
+          self.ubicacion=gpd.GeoDataFrame(ubicacion,geometry=gpd.points_from_xy(ubicacion.Longitud_X,ubicacion.Latitud_Y))
+          self.ubicacion.plot()
+
+
+Purga(DATA,EXCEL,metodo,limite,NR,EP).base()
+      
