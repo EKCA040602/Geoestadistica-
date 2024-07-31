6eb97477bb8e811bfb09ed18f753f5d95cf1e719
EKCA040602 <143982463+EKCA040602@users.noreply.github.com>
2024-07-31 00:30:05 -0500

    CORRELACION.py

--- CORRELACION.py
+++ CORRELACION.py
@@ -1,2 +1,4 @@
-from ELIMINACION_DE_OUTLIERS.py import cruda_log,sin_outliers
+from ELIMINACION_DE_OUTLIERS.py import cruda_log,sin_outliers,datos_normalizados
+#Lo primero que se va a hacer es discriminar la data importante.Es decir la que cumple con una distribucion normal
+
 
